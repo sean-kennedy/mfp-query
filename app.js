8b32e9d877babaf74fc85f9f576be9f6053e18bc
Sean Kennedy <s.kennedy@pni.com.au>
2017-02-20 15:13:22 +1000

    Cleaned up code and added wrapper object for metadata

--- app.js
+++ app.js
@@ -7,81 +7,87 @@ var express = require('express'),
 
 app.get('/', function(req, res) {
 
-	// Init vars
-	var	url = 'http://www.myfitnesspal.com/food/search',
-		userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:22.0) Gecko/20100101 Firefox/22.0';
-
 	if (!req.query.s) {
 		return res.json({error: 'No query sent'});
 	}
 
-	var page = req.query.page || 1;
+	var	url = 'http://www.myfitnesspal.com/food/search',
+		userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:22.0) Gecko/20100101 Firefox/22.0',
+		page = req.query.page || 1,
+		query = req.query.s;
 
-	request.post(url, { form: { search: req.query.s, page: page } }, function(err, r) {
+	request.post(url, { form: { search: query, page: page } }, function(error, data) {
 
-		if (err || r.statusCode !== 200) {
-			console.log(err);
-			return res.send({error: err});
+		if (error || data.statusCode !== 200) {
+
+			console.log(error);
+
+			return res.json({error: error});
 		}
 
-		var	$ = cheerio.load(r.body);
-
-		var foodList = $('.food_search_results li');
-
-		var allFood = [];
+		var	$ = cheerio.load(data.body),
+			responseObj = {},
+			foodList = $('.food_search_results li'),
+			results = $('.result_count').html().replace(/[\n]/g, ''),
+			formattedFoodList = [],
+			resultObj = {};
 
 		foodList.each(function(i, item) {
 
-			var info = $(item).find('.nutritional_info').html();
-			var desc = $(item).find('.food_description');
+			var formattedItem = {},
+				description = $(item).find('.food_description'),
+				nutritionalInfo = $(item).find('.nutritional_info').html().replace(/\t/g, '').split('\n');
 
-			var name1 = desc.find('a:nth-child(1)').html();
-			var name2 = desc.find('a:nth-child(2)').html();
+			formattedItem.name = description.find('a:nth-child(1)').html() + ' (' + description.find('a:nth-child(2)').html() + ')';
 
-			var splitter = info.replace(/\t/g, '').split('\n');
+			nutritionalInfo.forEach(function(infoItem) {
 
-			var nutObj = {
-				name: he.decode(name1 + ' (' + name2 + ')')
-			};
-
-			splitter.forEach(function(split, i) {
-
-				if (split) {
-
-					split = split.replace(',', '');
-					split = split.replace(':', '');
-
-					var value = split.replace(/<([^>]+?)([^>]*?)>(.*?)<\/\1>/ig, '');
-
-					value = he.decode(trim1(value));
-
-					var key;
-
-					split.replace(/<label>(.*?)<\/label>/g, function() {
-						key = trim1(arguments[1]).toLowerCase().replace(/ /g, '_');
-					});
-
-					if (key === 'calories') {
-
-						var kj = value * 4.184;
-
-						kj = kj.toFixed(1);
-
-						nutObj['kilojoules'] = kj;
-					}
-
-					nutObj[key] = value;
-
+				if (!infoItem) {
+					return;
 				}
+
+				var value,
+					key;
+
+				infoItem = infoItem.replace(/[,:]/g, '');
+
+				value = infoItem.replace(/<([^>]+?)([^>]*?)>(.*?)<\/\1>/ig, '');
+				value = he.decode(trimSpaceStartEnd(value));
+
+				infoItem.replace(/<label>(.*?)<\/label>/g, function() {
+					key = trimSpaceStartEnd(arguments[1]).toLowerCase().replace(/ /g, '_');
+				});
+
+				if (key === 'calories') {
+
+					var kj = value * 4.184;
+
+					formattedItem['kilojoules'] = parseFloat(kj.toFixed(1));
+				}
+
+				if (key !== 'serving_size') {
+					value = parseFloat(value);
+				}
+
+				formattedItem[key] = value;
 
 			});
 
-			allFood.push(nutObj);
+			formattedFoodList.push(formattedItem);
 
 		});
 
-		res.set('Content-Type', 'text/json');
-		res.json(allFood);
+		results = trimSpaceStartEnd(results).split(' ');
+
+		resultObj.total = parseInt(results.pop());
+		resultObj.num_results = formattedFoodList.length;
+
+		responseObj.query = query;
+		responseObj.page = parseInt(page);
+		responseObj.results = resultObj;
+		responseObj.data = formattedFoodList;
+
+		res.json(responseObj);
 
 	});
 
@@ -91,6 +97,6 @@ app.listen(port, function() {
 	console.log('Listening on ' + port);
 });
 
-function trim1 (str) {
+function trimSpaceStartEnd(str) {
     return str.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
 }
